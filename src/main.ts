c98d9f6efa7319e354142da589d0c5f346978a23
sharjeel-tplex <124800173+sharjeel-tplex@users.noreply.github.com>
2023-04-17 09:14:24 +0300

    Angular Dynamic template using dynamic component

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
   OnInit,
   ViewContainerRef,
   ɵcompileComponent,
+  ViewChild,
 } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -23,11 +24,11 @@ import { FormsModule } from '@angular/forms';
     <form>
     
     <label for="template">Template: </label>
-    <select name="cardDesign" [(ngModel)]="cardDesign">>
+    <select (ngModelChange)="onDropdownChange()" name="cardDesign" [(ngModel)]="cardDesign">>
+      <option value="Default">Default</option>
       <option value="Design1">Design 1</option>
       <option value="Design2">Design 2</option>
       <option value="Design3">Design 3</option>
-      <option value="Default">Defautl</option>
     </select>
     <br />
       <label for="fullname">Full Name: </label>
@@ -40,10 +41,12 @@ import { FormsModule } from '@angular/forms';
       <input type="text" id="phone" name="phone" [(ngModel)]="contact.phone"> <br />
 
   </form>
+
+  <ng-container #container></ng-container>
+
 `,
 })
 export class App implements OnInit {
-  name = 'Angular';
   cardDesign = 'Default';
   contact: Contact = new Contact();
   myValues = ['value1', 'value2'];
@@ -54,22 +57,79 @@ export class App implements OnInit {
     this.contact.phone = form.value['phone'];
   }
 
-  viewRef = inject(ViewContainerRef);
+  //viewRef = inject(ViewContainerRef);
+  @ViewChild('container', { read: ViewContainerRef })
+  container: ViewContainerRef;
 
   ngOnInit(): void {
-    //const template = `<h3>Hello World!</h3><div *ngFor="let item of items">{{ item }}</div>`;
-    const template = `
-    <div style="padding:50px; border: solid 2px; margin: 50px;">
-      <h3>{{ contact.fullname }}</h3>
-      <h4 *ngIf="contact.phone">Cell: {{ contact.phone }}</h4>
-      <h4 *ngIf="contact.email">Email: {{ contact.email }}</h4>
-    </div>
-    `;
-    const component = getComponentFromTemplate(template);
-    const componentRef = this.viewRef.createComponent(component);
+    this.onDropdownChange();
+  }
 
-    //componentRef.setInput('items', this.myValues);
-    componentRef.setInput('contact', this.contact);
+  onDropdownChange() {
+    // Destroy the current component
+    this.container.clear();
+    setTimeout((_) => {
+      const template = this.getDesignTemplate(this.cardDesign);
+      // Create the new component with the ComponentFactoryResolver
+      const component = getComponentFromTemplate(template);
+      const childComponentRef = this.container.createComponent(component);
+
+      // You can pass data to the child component using the component instance's properties or methods
+      childComponentRef.setInput('contact', this.contact);
+    }, 500);
+  }
+
+  getDesignTemplate(design) {
+    let template = '';
+
+    switch (design) {
+      case 'Default': {
+        template = `
+        <div style="padding:50px; border: solid 2px; margin: 50px;">
+          <h2>${this.cardDesign}</h2>  
+          <h3>{{ contact.fullname }}</h3>
+          <h4 *ngIf="contact.phone">Cell: {{ contact.phone }}</h4>
+          <h4 *ngIf="contact.email">Email: {{ contact.email }}</h4>
+        </div>
+        `;
+        break;
+      }
+      case 'Design1': {
+        template = `
+        <div style="padding:50px; border: dotted 3px blue; margin: 50px;">
+          <h2>${this.cardDesign}</h2>  
+          <h3>{{ contact.fullname }}</h3>
+          <h4 *ngIf="contact.email">Email: {{ contact.email }}</h4>
+          <h4 *ngIf="contact.phone">Cell: {{ contact.phone }}</h4>
+        </div>
+        `;
+        break;
+      }
+      case 'Design2': {
+        template = `
+        <div style="padding:50px; border: dashed 2px red; margin: 50px;">
+          <h2>${this.cardDesign}</h2>  
+          <h3>{{ contact.fullname }}</h3>
+          <h4 *ngIf="contact.phone">Cell: {{ contact.phone }}</h4>
+          <h4 *ngIf="contact.email">Email: {{ contact.email }}</h4>
+        </div>
+        `;
+        break;
+      }
+      case 'Design3': {
+        template = `
+        <div style="padding:50px; border: solid 4px green; margin: 50px;">
+          <h2>${this.cardDesign}</h2>  
+          <h3>{{ contact.fullname }}</h3>
+          <h4 *ngIf="contact.phone">Cell: {{ contact.phone }}</h4>
+          <h4 *ngIf="contact.email">Email: {{ contact.email }}</h4>
+        </div>
+        `;
+        break;
+      }
+    }
+
+    return template;
   }
 }
 
